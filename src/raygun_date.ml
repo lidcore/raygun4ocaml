35f29a5bee5ba75b108748356f64a31bce964c1f
Romain Beauxis <toots@rastageeks.org>
2015-10-26 15:32:10 -0500

    Implement date wrap.

--- src/raygun_date.ml
+++ src/raygun_date.ml
@@ -1,4 +1,4 @@
 type t = CalendarLib.Date.t
 let now = CalendarLib.Date.today
-let wrap = failwith "Not implemented!"
+let wrap = CalendarLib.Printer.Date.from_fstring "%F"
 let unwrap = CalendarLib.Printer.Date.sprint "%F"
