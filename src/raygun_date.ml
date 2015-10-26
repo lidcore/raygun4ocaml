8482fb0c945102b388bd3f5b0a171a9dcd144cc1
Romain Beauxis <toots@rastageeks.org>
2015-10-26 15:12:29 -0500

    Add Raygun_date.now

--- src/raygun_date.ml
+++ src/raygun_date.ml
@@ -1,3 +1,4 @@
 type t = CalendarLib.Date.t
+let now = CalendarLib.Date.today
 let wrap = failwith "Not implemented!"
 let unwrap = CalendarLib.Printer.Date.sprint "%F"
--- src/raygun_date.mli
+++ src/raygun_date.mli
@@ -1,3 +1,4 @@
 type t
+val now : unit -> t
 val unwrap : t -> string
 val wrap : string -> t
